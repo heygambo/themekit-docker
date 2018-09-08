6fecc5c8e053996d1ddfada26cdac51da50c0eee
Christian Gambardella <christian.gambardella@gmail.com>
2018-09-08 20:32:18 +0200

    builds and pushes :latest

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 VERSION=$(curl https://shopify-themekit.s3.amazonaws.com/releases/latest.json | jq -r .version)
 echo "Building $VERSION version."
-docker build -t gambo/themekit:$VERSION .
+docker build -t gambo/themekit:$VERSION -t gambo/themekit:latest .
 docker push gambo/themekit:$VERSION
 docker push gambo/themekit:latest
